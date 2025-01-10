d5d375f356e2f9e8e8a0b4eee08c4c9b4e42c8b9
BT_1N3_05 <williamb269@nycstudents.net>
2025-01-10 13:50:15 -0500

    cool beans

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Lev is a meanie");
         System.out.println("nah ");
-        //dont
+        System.out.println("Hullo!");
     }
 }
