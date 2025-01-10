4531d014c789e17416f8bb5561b3e6755a4204f3
BT_1N3_06 <levy@nycstudents.net>
2025-01-10 13:40:19 -0500

    sigma sigma boy

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         char x=0;
-        for (int i = 0; i < 5555; i++) {
+        for (int i = 0; i < 55555; i++) {
             x=(char)i;
             System.out.print(x);
-            if(i==100) System.out.println();
+            if((int)i%1000==0) System.out.println();
         }
     }
 }
