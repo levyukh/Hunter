a5b205231bae9602d6a12353612e6f9db3b46ab8
Williamb269 <williamb269@nycstudents.net>
2025-01-10 13:49:13 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Lev is a meanie");
         System.out.println("nah ");
-        char x=0;
-        for (int i = 0; i < 555554; i++) {
-            x=(char)i;
-            System.out.print(x);
-            if((int)i%1000==0) System.out.println();
-        }
+        //dont
     }
 }
