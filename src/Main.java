f701b322639832376761ee24af43390b9a32788d
BT_1N3_06 <levy@nycstudents.net>
2025-01-10 13:38:59 -0500

    sigma sigma boy

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        char x=0;
+        for (int i = 0; i < 5555; i++) {
+            x=(char)i;
+            System.out.print(x);
+            if(i==100) System.out.println();
+        }
+    }
+}
