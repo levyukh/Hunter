6522f7853c888b0240713ee097e235a38bdbfcde
BT_1N3_06 <levy@nycstudents.net>
2025-01-10 13:48:26 -0500

    raorrnflk

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Lev is a meanie");
+        System.out.println("nah ");
         char x=0;
         for (int i = 0; i < 555554; i++) {
             x=(char)i;
